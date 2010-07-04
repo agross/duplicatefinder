c60d3031582fd846e9218ca57fb7bff3be9b554d
Alexander Groß <agross@therightstuff.de>
2010-07-04 13:40:56 +0200

    Added projects to split up logic and console application

---
+++ source/SolutionInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyCompany("Alexander Groß")]
+[assembly: AssemblyProduct("DuplicateFinder")]
+[assembly: AssemblyCopyright("Copyright © Alexander Groß")]
